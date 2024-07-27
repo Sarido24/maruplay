1a4e6bd7dc36888b00980bdb703945250f3a711a
SARIDO_SIMANULLANG <sarido24@gmail.com>
2024-07-27 16:32:22 +0700

    fix: fix the position of the components folder in  the project

---
+++ maruplay/src/pages/RegisterPageSeller.jsx
@@ -0,0 +1,5 @@
+export default function RegisterPageSeller(){
+    return(
+        <>Register Seller</>
+    )
+}
---
+++ maruplay/src/pages/RegisterPageUser.jsx
@@ -0,0 +1,5 @@
+export default function RegisterPageUser(){
+    return(
+        <>Register User</>
+    )
+}
